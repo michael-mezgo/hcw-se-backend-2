8e7ec0a3ab0ab18b37092b892f119d28e91031e3
Michael <58122000+michael-mezgo@users.noreply.github.com>
2026-05-04 16:45:00 +0200

    Allow post

--- booking-service/src/main/kotlin/Http.kt
+++ booking-service/src/main/kotlin/Http.kt
@@ -12,6 +12,8 @@ fun Application.configureHttp() {
         allowMethod(HttpMethod.Put)
         allowMethod(HttpMethod.Delete)
         allowMethod(HttpMethod.Patch)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Get)
         allowHeader(HttpHeaders.Authorization)
         allowHeader("MyCustomHeader")
         anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
